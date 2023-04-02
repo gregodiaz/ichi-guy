00021a5e95eb19b4fff86d90956af441965410a8
gregodiaz <diazgregoriocruz@gmail.com>
2023-04-02 19:43:40 -0300

    added: implements vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types='vitest' />
+/// <reference types='vite/client' />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -5,4 +8,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
 	plugins: [react()],
 	base: '/ichi-guy/',
+	test: {
+		environment: 'jsdom',
+		globals: true
+	}
 })
